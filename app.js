de08edcc99546216fa78c8997608ab55bf5ef5fd
isaiasbrianojasso <isaias.briano@gmail.com>
2019-02-28 18:19:50 -0600

    examen

--- app.js
+++ app.js
@@ -1,146 +1,76 @@
-///////////////////////////////////////INICIALIZA EXPRESS//////////////////////////////////////
-const express = require("express");
+const express =  require("express");
 const bodyParser = require("body-parser");
 var app = express();
-///////////////////////////////////////REQUIERE MODIFICAR ARCHIVOS//////////////////////////////////////
-const archivos = require('fs'); //FILE SYSTEM
-///////////////////////////////////////DB HANDLER//////////////////////////////////////
+
+const archivos = require('fs');
+
+
+//DB Handler
 var db = {
-  ///////////////////////////////////////ABRIR  CONEXION //////////////////////////////////////
-  initDB: function () {
-    var fs = require("fs");
-    var contents = fs.readFileSync("./alumnos.json");
-    this.alumnos = JSON.parse(contents);
-  },
-  ///////////////////////////////////////BUSQUEDA ALUMNOS//////////////////////////////////////
-  getAlumnoBy: function (filter, value) {
-    console.log("filtro: " + filter + "valor: " + value);
-    var selected = null;
-    this.alumnos.forEach(alumno => {
-      console.log(alumno);
-      console.log(alumno[filter]);
-      if (alumno[filter] == value) {
-        selected = alumno;
+    //Indicar BD o abrir conexion
+    initDB: function () {
+        var fs = require("fs");
+        var contents = fs.readFileSync("./youtubes.json");
+        this.youtubes = JSON.parse(contents);
+    },
+
+    //Busqueda Alumno
+    getyoutubesBy: function (filter, value) {
+        console.log("filtro: " + filter + "valor: " + value);
+        var selected = null;
+        this.youtubes.forEach(youtubes => {
+            console.log(youtubes);
+            console.log(youtubes[filter]);
+            if (youtubes[filter] == value) {
+                selected = youtubes;
+                return selected;
+            }
+        });
         return selected;
-      }
-    });
-    return selected;
-  },
+    },
 
-  ///////////////////////////////////////Buscar ALUMNOS//////////////////////////////////////
-
-
-
-  ///////////////////////////////////////ELIMINAR ALUMNOS//////////////////////////////////////
-  //Eliminar un alumno por la clave
-  deleteAlumnoByClave: function (clave) {
-    var index;
-    //Buscamos el indice del alumno
-    for (index = 0; index < this.alumnos.length; index++) {
-      if (this.alumnos[index].clave == clave)
-        break;
+    saveyoutubes : function(){
+      archivos.writeFileSync('youtubes.json', JSON.stringify(this.youtube),
+        function (error) {
+            if (error) {
+                console.log('Hubo un error al escribir en el archivo')
+                console.log(error);
+            }
+        });
     }
-    //Si se encontro el indice se elimina
-    if (index < db.alumnos.length) {
-      delete db.alumnos[index];
-      db.alumnos.splice(index, 1);
-      db.saveAlumnos();
-    }
-  },
-  ///////////////////////////////////////EDITAR ALUMNOS//////////////////////////////////////
-  editAlumnoByClave: function (alumno) {
-    //Buscamos el indice del alumno
-    for (var index = 0; index < this.alumnos.length; index++) {
-      if (this.alumnos[index].clave == alumno.claveOld) {
-        console.log("alumno encontrado.");
-        this.alumnos[index].clave = alumno.claveNew;
-        this.alumnos[index].nombre = alumno.nombreNew;
-        break;
-      }
-    }
-    console.log(this.alumnos)
-    db.saveAlumnos();
-  },
-
-  editAlumno: function (alumno) {
-    //Buscamos el indice del alumno
-    for (var index = 0; index < this.alumnos.length; index++) {
-      if (this.alumnos[index].clave == alumno.claveOld) {
-        console.log("alumno encontrado.");
-        this.alumnos[index].clave = alumno.claveNew;
-        this.alumnos[index].nombre = alumno.nombreNew;
-        break;
-      }
-    }
-    console.log(this.alumnos)
-    db.saveAlumnos();
-  },
-  ///////////////////////////////////////GUARDAR ALUMNOS//////////////////////////////////////
-
-  saveAlumnos: function () {
-    archivos.writeFileSync('alumnos.json', JSON.stringify(this.alumnos),
-      function (error) {
-        if (error) {
-          console.log('Hubo un error al escribir en el archivo')
-          console.log(error);
-        }
-      });
-  }
-  ///////////////////////////////////////FINALIZA GUARDAR ALUMNOS//////////////////////////////////////
+    
 }
-///////////////////////////////////////FINALIZA DB HANDLER//////////////////////////////////////
 
-
-///////////////////////////////////////AQUI SE RECIBEN LOS POST,DELETE Y PUT//////////////////////////////////////
 app.use(express.static('assets'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.get('/', function (req, res) {
-  res.sendfile("index.html");
+
+app.get('/',function(req,res){
+  res.sendfile("index.html" );
 });
 
-app.route("/alumnos")
-  .get((req, res) => {
-    db.initDB();
-    res.json(db.alumnos);
-  })
-
-  .post((req, res) => {
-    db.initDB();
-    var alumno = req.body;
-    console.log("Objeto post recibido");
-    console.log(alumno);
-    db.alumnos.push(alumno);
-    db.saveAlumnos();
-    res.json({ 'status': 'OK' });
-  })
-
-  .put((req, res) => {
-    db.initDB();
-    var alumno = req.body;
-    console.log("Objeto put recibido");
-    console.log(alumno);
-    db.editAlumno(alumno);
-    res.json({ 'status': 'OK' })
-  })
-
-  .delete((req, res) => {
-    db.initDB();
-    var alumno = req.body;
-    console.log("Objeto delete recibido");
-    console.log(alumno);
-    db.deleteAlumnoByClave(alumno.clave);
-    console.log(db.alumnos);
-    res.json({ 'status': 'OK' })
-  });
-
-app.get('/alumnos/:clave', (req, res) => {
+app.get('/youtubes', (req, res) => {
   db.initDB();
-  var clave = req.params.clave;
-  var alumno = db.getAlumnoBy('clave', clave);
-  res.json(alumno);
+  res.json(db.youtubes);
 });
 
-app.listen(3000, function () {
+app.get('/youtubes/:kind', (req, res) => {
+  db.initDB();
+  var kind = req.params.kind;
+  var youtubes = db.getyoutubesBy('kind', kind);
+  res.json(youtubes);
+});
+
+app.post('/youtubes',function(req,res){
+  db.initDB();
+  var youtubes = req.body;
+  console.log("Objeto post recibido");
+  console.log(youtubes);
+  db.youtubes.push(youtubes);
+  db.saveyoutubes();
+  res.json({'status' : 'OK'});
+});
+
+app.listen(3000,function(){
   console.log("Started on PORT 3000");
 })
